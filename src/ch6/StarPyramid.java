72b2eab78525e1da45fcbcb997719443ac450a24
John <1996tjrgns@naver.com>
2022-11-02 15:59:30 +0900

    commit

---
+++ src/ch6/StarPyramid.java
@@ -0,0 +1,33 @@
+package ch6;
+
+public class ssssssssssssssssssssssssssss {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 5; i++) {
+            ////54321
+            //문자 두가지를 n번씩 찍으므로 for 두가지
+            for (int j = 5; j > i; j--) {
+                System.out.print("_");
+            } //이번행 _ 입력 끝
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            } //이번행 * 입력 끝
+            System.out.println(); //줄바꿈
+            //어렵다 ㅅㅂ
+        }
+
+
+
+
+            /*
+                    for (int i = 5; i >= 1; i--) {
+                        for (int j = 5; j >= 1; j--) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+            */
+
+    }
+}
+
---
+++ src/ch8/WhatYourName.java
@@ -0,0 +1,2 @@
+package ch8;public class WhatYourName {
+}
